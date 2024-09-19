9de25a29d03672f80d913fc4cba06eeb20a01a17
luispiedrahita258 <lepiedrahita5@misena.edu.co>
2024-09-18 21:13:25 -0500

    Agrega configuración de Firebase Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC177Aw4c7SQaMs9ne7vR-ANaP3mhhEoU",
+    authDomain: "pelis-clone.firebaseapp.com",
+    projectId: "pelis-clone",
+    storageBucket: "pelis-clone.appspot.com",
+    messagingSenderId: "203835116202",
+    appId: "1:203835116202:web:7336e74f123a043b385ed3"
+  }
 };
